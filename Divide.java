6e1257a57b912232d6c561d24f6caababb09c563
Rishabh Kumar <rishabhkumar1024@gmail.com>
2022-08-03 10:56:34 +0530

    bug fixed

--- Divide.java
+++ Divide.java
@@ -3,6 +3,6 @@ public class Divide {
     public static void main(String[] args) {
         int a = 5;
         int b  = 6;
-        System.out.println("The division is :- " + (a / b));
+        System.out.println("The division is aaa :- " + (a / b));
     }
 }
