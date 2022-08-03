eb0462b3bd24e939f761937d8de81f33dadd47b9
Rishabh Kumar <rishabhkumar1024@gmail.com>
2022-08-03 10:48:50 +0530

    added divide

---
+++ Divide.java
@@ -0,0 +1,8 @@
+public class Divide {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b  = 6;
+        System.out.println("The division is :- " + (a / b));
+    }
+}
